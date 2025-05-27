b319da8ebba95b2ce7d8ea4ca7c09e91425e4eca
Grumble <hujik1234@gmail.com>
2025-05-27 23:29:37 +0900

    feat: 상품 등록 기능 구현 (@ModelAttribute 생략 케이스 포함)
    - @ModelAttribute에 name을 명시한 경우 자동 모델 등록 처리
    - name 생략 시 클래스명을 기반으로 model 속성 자동 설정 (Item → item)
    - @ModelAttribute 전체 생략 시에도 동일하게 모델 등록 처리됨
    - 최종적으로 상품 저장 후 상세 페이지로 이동

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +28,53 @@ public class BasicItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    // @ModelAttribute의 name을 설정하면 굳이 추가를 해주지 않아도 자동으로 추가되어, 생략이 가능하다.
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    // name을 생략하면 클래스명의 앞글자만 소문자로 해서 addAttribute가 된다.
+    // Item -> item
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    // @ModelAttribute를 생략해도 결국 위와 같은 애노테이션이 붙은 것처럼 동작하는 것이기 때문에 자동등록도 똑같이 동작한다.
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
         return "basic/item";
     }
 
